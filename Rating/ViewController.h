f4b1bad503307f09e468ebaeb1017a4eb71267c5
liumn <liumn@polyvi.com>
2014-03-03 15:03:33 +0800

    Initial Commit

---
+++ Rating/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rating
+//
+//  Created by zoe on 14-3-3.
+//  Copyright (c) 2014年 Polyvi Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rating/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Rating
+//
+//  Created by zoe on 14-3-3.
+//  Copyright (c) 2014年 Polyvi Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
